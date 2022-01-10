32157aa8910fe70c4ebd510e501b917ece1097d2
WilliamsTPL <williams342002@gmail.com>
2022-01-10 10:36:32 +0700

    Login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    
+        return $user->createToken($user->id)->plainTextToken;
+
+        // return 'success';
+    }
+}
